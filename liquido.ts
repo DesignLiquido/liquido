d257dc898a07ba588edbcba2a013b629c402fbf6
ItaloCobains <monokatchaleger@gmail.com>
2022-11-17 21:25:02 -0300

    Atualizando metodo de descobrir rotas.

--- liquido.ts
+++ liquido.ts
@@ -1,8 +1,6 @@
 import * as caminho from "path";
 import * as SistemaDeArquivo from "node:fs";
 
-import { filtroPerformatico } from "./utilidades/filtro-performatico";
-
 import {
   AvaliadorSintatico,
   Importador,
@@ -35,8 +33,8 @@ export class Liquido {
   roteador: Roteador;
 
   arquivosDelegua: Array<string>;
-  caminhoAbsoluto: string = caminho.join(__dirname, "rotas");
-  subPastasEmRotas: Array<string>;
+  diretorioBase: string = __dirname;
+  diretorioDescobertos: string[] = [];
 
   arquivosAbertos: { [identificador: string]: string };
   conteudoArquivosAbertos: { [identificador: string]: string[] };
@@ -62,38 +60,36 @@ export class Liquido {
     this.roteador = new Roteador(this.conversorLmht);
   }
 
-  descobrirRotas(pasta: string = ""): void {
-    const ListaDeItems = SistemaDeArquivo.readdirSync(
-      caminho.join(this.caminhoAbsoluto, pasta)
-    );
+  // recursaoDescobertaRotas(diretorio: string): void {
+  //   this.rotasDescobertas.forEach((diretorio) => {
+  //     this.descobrirRotas(diretorio);
+  //   });
+  // }
 
-    ListaDeItems.forEach((e) => {
-      if (e.endsWith(".delegua")) {
-        this.arquivosDelegua.push(e);
+  descobrirRotas(diretorio: string): void {
+    const ListaDeItems = SistemaDeArquivo.readdirSync(diretorio);
+
+    const diretorioDescobertos = [];
+
+    ListaDeItems.forEach((diretorioOuArquivo) => {
+      const caminhoAbsoluto = caminho.join(diretorio, diretorioOuArquivo);
+      if (caminhoAbsoluto.endsWith(".delegua")) {
+        this.arquivosDelegua.push(caminhoAbsoluto);
         return;
       }
-      if (
-        SistemaDeArquivo.lstatSync(
-          this.caminhoAbsoluto + `\\${e}`
-        ).isDirectory()
-      ) {
-        this.subPastasEmRotas.push(e);
+      if (SistemaDeArquivo.lstatSync(caminhoAbsoluto).isDirectory()) {
+        diretorioDescobertos.push(caminhoAbsoluto);
         return;
       }
     });
-    if (this.subPastasEmRotas.length > 0) {
-      this.subPastasEmRotas.forEach((e) => {
-        const indice = this.subPastasEmRotas.indexOf(e);
-        this.subPastasEmRotas.splice(indice, 1);
-        this.descobrirRotas(e);
-      });
-    }
+    diretorioDescobertos.forEach((diretorioDescoberto) => {
+      this.descobrirRotas(diretorioDescoberto);
+    });
   }
 
   importarArquivoRota(caminhoRelativo: string) {
     this.arquivosDelegua = [];
-    this.subPastasEmRotas = [];
-    this.descobrirRotas();
+    this.descobrirRotas(caminho.join(this.diretorioBase, "rotas"));
 
     const retornoImportador = this.importador.importar(this.arquivosDelegua[0]); // chumbei aqui para teste
 
