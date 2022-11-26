40b8c1a3bbdbd22689846f8d94ed06749865f269
ItaloCobains <monokatchaleger@gmail.com>
2022-11-26 14:15:55 -0300

    Configurando testes.

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types';
+export default async (): Promise<Config.InitialOptions> => {
+	return {
+		preset: 'ts-jest',
+		displayName: {
+			name: 'liquido',
+			color: 'greenBright'
+		},
+		verbose: true,
+		setupFiles: ['dotenv/config'],
+		testMatch: ['**/**/*.test.ts'],
+		testEnvironment: 'node',
+		detectOpenHandles: true,
+		collectCoverage: true,
+		transform: { '^.+\\.tsx?$': 'ts-jest' },
+		globalTeardown: '<rootDir>/testes/jest-globals-teardown.ts',
+		forceExit: true
+	};
+};
