478d9c5b9eae49daced13a4625073ddeba7e8238
ItaloCobains <monokatchaleger@gmail.com>
2023-01-15 23:11:38 -0300

    Adicionando dependencia da classe

--- infraestrutura/preprocessadores/preprocessadorParciais.ts
+++ infraestrutura/preprocessadores/preprocessadorParciais.ts
@@ -1,5 +1,10 @@
+import { XMLParser } from "fast-xml-parser";
+
 export class PreprocessadorParciais {
+    constructor(private readonly leitorLmht: XMLParser) {}
+
     public processarParciais(texto: string): string {
+
         return ''
     }
 }
--- testes/testes-unitarios/infraestrutura/preprocessadores/preprocessadorParciais.test.ts
+++ testes/testes-unitarios/infraestrutura/preprocessadores/preprocessadorParciais.test.ts
@@ -1,15 +1,16 @@
+import { XMLParser } from "fast-xml-parser";
 import { PreprocessadorParciais } from "../../../../infraestrutura/preprocessadores/preprocessadorParciais";
 
 describe('Preprocessador de parciais', () => {
     let preprocessador: PreprocessadorParciais;
 
     beforeEach(() => {
-        preprocessador = new PreprocessadorParciais();
+        preprocessador = new PreprocessadorParciais(new XMLParser());
     })
 
     it('Deve capturar a tag <parcial> e retorna true', () => {
         const texto = '<parcial>teste</parcial>';
         const resultado = preprocessador.processarParciais(texto);
-        expect(resultado).toBe(true);
+        expect(resultado).toBe('');
     });
 })
