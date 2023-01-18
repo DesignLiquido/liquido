6ac99de53f2df77394072fcdd6585c446ab5c5a9
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-01-18 12:40:41 -0800

    Favor não usar global teardown. Dá problema com o CI.

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,16 @@
 import type { Config } from '@jest/types';
 export default async (): Promise<Config.InitialOptions> => {
 	return {
-		preset: 'ts-jest',
+		setupFiles: ['dotenv/config'],
+        verbose: true,
+        modulePathIgnorePatterns: ['<rootDir>/dist/'],
+        preset: 'ts-jest',
+        testEnvironment: 'node',
+        coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary'],
 		displayName: {
 			name: 'liquido',
 			color: 'greenBright'
 		},
-		verbose: true,
-		setupFiles: ['dotenv/config'],
-		testMatch: ['**/**/*.test.ts'],
-		testEnvironment: 'node',
 		detectOpenHandles: true,
-		transform: { '^.+\\.tsx?$': 'ts-jest' },
-		globalTeardown: '<rootDir>/testes/jest-globals-teardown.ts',
-		forceExit: true,
-		coverageReporters: ['json-summary', 'lcov', 'text', 'text-summary']
-	};
+    };
 };
--- testes/jest-globals-teardown.ts
+++
@@ -1,3 +0,0 @@
-export default () => {
-	process.exit(0);
-};
