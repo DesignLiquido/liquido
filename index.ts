991145511077267ff416a0120867a4ea62191f2f
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-11-07 14:15:09 -0800

    Comandos para:
    - Executar o servidor local;
    - ILC, ou CLI em inglês.

--- index.ts
+++ index.ts
@@ -1,4 +1,27 @@
 import { Liquido } from './liquido';
+import { Command } from 'commander';
 
-const liquido = new Liquido(process.cwd());
-liquido.iniciar();
+const pontoDeEntrada = async () => {
+    const analisadorArgumentos = new Command();
+    analisadorArgumentos;
+
+    analisadorArgumentos
+        .helpOption('-h, --ajuda', 'Exibe a ajuda para o comando.');
+
+    analisadorArgumentos
+        .command('servidor', 'Serve o diretório local como uma aplicação para a internet.', { isDefault: true })
+        .action(() => {
+            const liquido = new Liquido(process.cwd());
+            liquido.iniciar();
+        });
+
+    analisadorArgumentos
+        .command('novo <nome>', 'Inicia uma nova aplicação pré-configurada para funcionar com Liquido.')
+        .action((nome: string) => {
+            console.log(nome);
+        });
+
+    analisadorArgumentos.parse();
+}
+
+pontoDeEntrada();
