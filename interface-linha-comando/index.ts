bc9d57ed69e6be5ce513b198a1d1c80a81e44524
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-11-07 22:02:52 -0800

    Procedimento de cópia de exemplos para diretórios de projeto.

--- interface-linha-comando/index.ts
+++ interface-linha-comando/index.ts
@@ -1,3 +1,4 @@
+import { async as glob } from 'fast-glob'
 import sistemaArquivos from 'fs';
 import caminho from 'path';
 
@@ -11,6 +12,27 @@ export function criarDiretorioAplicacao(nomeAplicacao: string): string {
     }
 
     return diretorioCompleto;
+}
+
+export async function copiarExemploParaProjeto(tipoDeProjeto: string, diretorioProjeto: string) {
+    const diretorioExemplos = caminho.join(__dirname, '../exemplos/' + tipoDeProjeto);
+    const formatoGlob = (diretorioExemplos + '/**/*.{delegua,foles,lmht,md}').replace(/\\/gi, '/');
+    
+    const arquivos = await glob([formatoGlob], {
+        dot: true,
+        absolute: false,
+        stats: false,
+    });
+
+    return Promise.all(
+        arquivos.map(async (arquivo) => {
+            const arquivoResolvido = caminho.resolve(arquivo);
+            const novoCaminhoArquivo = arquivoResolvido.replace(diretorioExemplos, diretorioProjeto);
+            console.log(novoCaminhoArquivo);
+            await sistemaArquivos.promises.mkdir(caminho.dirname(novoCaminhoArquivo), { recursive: true })
+            return sistemaArquivos.promises.copyFile(arquivoResolvido, novoCaminhoArquivo);
+        })
+    );
 }
 
 /* export function gerarProjetoPorTipoDeProjeto(tipoDeProjeto: string) {
