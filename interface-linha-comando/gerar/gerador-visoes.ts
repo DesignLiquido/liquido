291c6cb19be2433fc0c9a69b72dddd73a1bf192c
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-01-25 14:51:46 -0800

    Ajustes nos geradores de visões e rotas.

--- interface-linha-comando/gerar/gerador-visoes.ts
+++ interface-linha-comando/gerar/gerador-visoes.ts
@@ -4,6 +4,7 @@ import * as caminho from 'path';
 import { Classe } from '@designliquido/delegua/fontes/declaracoes';
 
 import { TipoVisao } from './tipo-visao';
+import { pluralizar } from '@designliquido/flexoes';
 
 export class GeradorVisoes {
     indentacao: number;
@@ -48,7 +49,7 @@ export class GeradorVisoes {
             case 'excluir':
                 caminhoVisao = caminho.join(diretorioVisoes, 'excluir.lmht');
                 cabecalho = " ".repeat(this.indentacao) + `<cabeça><título>Confirmar Exclusão de ${declaracaoModelo.simbolo.lexema}</título></cabeça>\n`;
-                corpo = '    <corpo>Teste</corpo>\n';
+                corpo = `${" ".repeat(this.indentacao)}<corpo>\n${this.corpoConfirmarExclusao(declaracaoModelo)}\n${" ".repeat(this.indentacao)}</corpo>\n`;
                 break;
         }
 
@@ -75,6 +76,7 @@ export class GeradorVisoes {
             colunasCabecaTabela.reduce(
                 (acumulador, elemento) => acumulador + '\n' + elemento
             ) +
+            '\n' + " ".repeat(this.indentacao * 5) + `<célula>Ações</célula>` +
             '\n' + " ".repeat(this.indentacao * 4) + '</linha>';
 
         const cabecaTabela = `${" ".repeat(this.indentacao * 3)}<cabeça-tabela>\n${linhaCabecaTabela}\n${" ".repeat(this.indentacao * 3)}</cabeça-tabela>`;
@@ -89,6 +91,11 @@ export class GeradorVisoes {
             colunasCorpoTabela.reduce(
                 (acumulador, elemento) => acumulador + '\n' + elemento
             ) +
+            '\n' + " ".repeat(this.indentacao * 5) + `<célula>` +
+            '\n' + `${" ".repeat(this.indentacao * 6)}<ligação destino="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}">Detalhes</ligação> |` +
+            '\n' + `${" ".repeat(this.indentacao * 6)}<ligação destino="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/editar">Editar</ligação> |` +
+            '\n' + `${" ".repeat(this.indentacao * 6)}<ligação destino="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/excluir">Excluir</ligação>` +
+            `\n${" ".repeat(this.indentacao * 5)}</célula>` +
             '\n' + " ".repeat(this.indentacao * 4) + '</linha>';
 
         const corpoTabela = `${" ".repeat(this.indentacao * 3)}<corpo-tabela>\n` +
@@ -100,6 +107,18 @@ export class GeradorVisoes {
         return `${" ".repeat(this.indentacao * 2)}<tabela>\n${cabecaTabela}\n${corpoTabela}\n${" ".repeat(this.indentacao * 2)}</tabela>`;
     }
 
+    private geracaoComumCamposDetalhes(declaracaoModelo: Classe): string {
+        const listaPropriedades: string[] = [];
+        for (const propriedade of declaracaoModelo.propriedades) {
+            listaPropriedades.push(" ".repeat(this.indentacao * 3) + `<termo>${propriedade.nome.lexema}</termo>`);
+            listaPropriedades.push(" ".repeat(this.indentacao * 3) + `<definição>{{${propriedade.nome.lexema}}}</definição>`);
+        }
+
+        return listaPropriedades.reduce(
+            (acumulador, elemento) => acumulador + '\n' + elemento
+        );
+    }
+
     /**
      * Função que gera o corpo de `detalhes.lmht` de cada visão gerada por linha de comando.
      * @param {Classe} declaracaoModelo A declaração do modelo de dados, com suas propriedades e definições.
@@ -108,26 +127,20 @@ export class GeradorVisoes {
     private corpoDetalhes(declaracaoModelo: Classe): string {
         const titulo = `${" ".repeat(this.indentacao * 2)}<titulo1>Detalhes de ${declaracaoModelo.simbolo.lexema}</titulo1>\n`;
 
-        const listaPropriedades: string[] = [];
-        for (const propriedade of declaracaoModelo.propriedades) {
-            listaPropriedades.push(" ".repeat(this.indentacao * 3) + `<termo>${propriedade.nome.lexema}</termo>`);
-            listaPropriedades.push(" ".repeat(this.indentacao * 3) + `<definição>{{${propriedade.nome.lexema}}}</definição>`);
-        }
-
         const relacaoPropriedades = `${" ".repeat(this.indentacao * 2)}<lista-definições>\n` + 
-            listaPropriedades.reduce(
-                (acumulador, elemento) => acumulador + '\n' + elemento
-            ) +
-            `\n${" ".repeat(this.indentacao * 2)}</lista-definições>\n`;
+            this.geracaoComumCamposDetalhes(declaracaoModelo) +
+            `\n${" ".repeat(this.indentacao * 2)}</lista-definições>\n\n` +
+            `${" ".repeat(this.indentacao * 2)}<ligação destino="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/editar">Editar</ligação> |\n` +
+            `${" ".repeat(this.indentacao * 2)}<ligação destino="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/excluir">Excluir</ligação>\n` ;
 
         return `${titulo}${relacaoPropriedades}`;
     }
 
-    private geracaoComumCaposFormulario(declaracaoModelo: Classe): string {
+    private geracaoComumCamposFormulario(declaracaoModelo: Classe): string {
         const listaPropriedades: string[] = [];
         for (const propriedade of declaracaoModelo.propriedades) {
             listaPropriedades.push(" ".repeat(this.indentacao * 4) + `<etiqueta para="${propriedade.nome.lexema}">${propriedade.nome.lexema}</etiqueta>`);
-            listaPropriedades.push(" ".repeat(this.indentacao * 4) + `<campo tipo="texto" id="{{${propriedade.nome.lexema}}}"></campo>`);
+            listaPropriedades.push(" ".repeat(this.indentacao * 4) + `<campo tipo="texto" id="${propriedade.nome.lexema}" value={{${propriedade.nome.lexema}}}></campo>`);
         }
 
         return listaPropriedades.reduce(
@@ -144,11 +157,12 @@ export class GeradorVisoes {
         const titulo = `${" ".repeat(this.indentacao * 2)}<titulo1>Adicionar ${declaracaoModelo.simbolo.lexema}</titulo1>\n`;
 
         const relacaoPropriedades = `${" ".repeat(this.indentacao * 3)}<campos>\n` + 
-            this.geracaoComumCaposFormulario(declaracaoModelo) +
+            this.geracaoComumCamposFormulario(declaracaoModelo) +
             `\n${" ".repeat(this.indentacao * 3)}</campos>\n`;
 
-        const formulario = `${" ".repeat(this.indentacao * 2)}<formulário método="POST" ação="">\n` + 
+        const formulario = `${" ".repeat(this.indentacao * 2)}<formulário método="POST" ação="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}">\n` + 
             `${relacaoPropriedades}` +
+            `\n\n${" ".repeat(this.indentacao * 3)}<campo tipo="enviar">Adicionar</campo>\n` +
             `${" ".repeat(this.indentacao * 2)}</formulário>\n`;
         return `${titulo}${formulario}`;
     }
@@ -162,13 +176,35 @@ export class GeradorVisoes {
         const titulo = `${" ".repeat(this.indentacao * 2)}<titulo1>Editar ${declaracaoModelo.simbolo.lexema}</titulo1>\n`;
 
         const relacaoPropriedades = `${" ".repeat(this.indentacao * 3)}<campos>\n` + 
-            `${" ".repeat(this.indentacao * 3)}<campo tipo="escondido" id="id"></campo>\n` +
-            this.geracaoComumCaposFormulario(declaracaoModelo) +
-            `\n${" ".repeat(this.indentacao * 2)}</campos>\n`;
+            `${" ".repeat(this.indentacao * 4)}<campo tipo="escondido" id="id"></campo>\n` +
+            this.geracaoComumCamposFormulario(declaracaoModelo) +
+            `\n\n${" ".repeat(this.indentacao * 4)}<campo tipo="enviar">Atualizar</campo>\n` +
+            `\n${" ".repeat(this.indentacao * 3)}</campos>\n`;
 
-        const formulario = `${" ".repeat(this.indentacao * 2)}<formulário método="POST" ação="">\n`+ 
+        const formulario = `${" ".repeat(this.indentacao * 2)}<formulário método="POST" ação="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/editar">\n`+ 
             `${relacaoPropriedades}` +
             `${" ".repeat(this.indentacao * 2)}</formulário>\n`;
         return `${titulo}${formulario}`;
+    }
+
+    /**
+     * Função que gera o corpo de `excluir.lmht` de cada visão gerada por linha de comando.
+     * @param {Classe} declaracaoModelo A declaração do modelo de dados, com suas propriedades e definições.
+     * @returns {string} Um trecho em LMHT com a estrutura do corpo da página.
+     */
+    private corpoConfirmarExclusao(declaracaoModelo: Classe): string {
+        const titulo = `${" ".repeat(this.indentacao * 2)}<titulo1>Confirmar Exclusão de ${declaracaoModelo.simbolo.lexema}</titulo1>\n`;
+        const mensagemConfirmacaoExclusao = `${" ".repeat(this.indentacao * 2)}<titulo3>Tem certeza de que deseja excluir o registro abaixo?</titulo3>\n`;
+
+        const relacaoPropriedades = `${" ".repeat(this.indentacao * 2)}<lista-definições>\n` + 
+            this.geracaoComumCamposDetalhes(declaracaoModelo) +
+            `\n${" ".repeat(this.indentacao * 2)}</lista-definições>\n`;
+
+        const formulario = `${" ".repeat(this.indentacao * 2)}<formulário método="POST" ação="/${pluralizar(declaracaoModelo.simbolo.lexema.toLocaleLowerCase())}/{{id}}/excluir">\n`+
+            `${" ".repeat(this.indentacao * 3)}<campo tipo="escondido" id="id" value={{id}}></campo>\n` +
+            `${" ".repeat(this.indentacao * 3)}<campo tipo="enviar">Confirmar Exclusão</campo>\n` +
+            `${" ".repeat(this.indentacao * 2)}</formulário>\n`;
+
+        return `${titulo}${mensagemConfirmacaoExclusao}${relacaoPropriedades}${formulario}`;
     }
 }
