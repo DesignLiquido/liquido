044441b724ad2682c7e43d905bfee1bcd85f3aa3
ItaloCobains <italobrandao.contato@gmail.com>
2024-05-07 23:23:30 -0300

    Adicionando suporte a version e description

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
-import { Command } from 'commander';
+import { textSync } from 'figlet'
+import { Command } from 'commander'
+import {version} from './package.json'
 
 const pontoDeEntrada = async () => {
+    console.log(textSync("Delegua"))
     const analisadorArgumentos = new Command();
-    analisadorArgumentos;
 
     analisadorArgumentos
+        .version(version)
+        .description("Conjunto de ferramentas para desenvolvimento de aplicações para a internet 100% em português.")
         .helpOption('-?, --ajuda', 'Exibe a ajuda para o comando.')
         .command('servidor', 'Serve o diretório local como uma aplicação para a internet.', { isDefault: true })
         .command('novo [nome]', 'Inicia uma nova aplicação pré-configurada para funcionar com Liquido.')
         .command('gerar [modelo]', 'Gera controlador e visão correspondentes ao nome do modelo passado por parâmetro. O modelo deve ter um arquivo .delegua correspondente no diretório "modelos".');
 
-    analisadorArgumentos.parse();
+    analisadorArgumentos.parse(process.argv);
 }
 
 pontoDeEntrada();
