b5154110dcb47a2e49c03402fc1dec6c4640de73
VictorFrancelino <victor.francelino10@gmail.com>
2026-03-27 12:35:56 -0400

    fix(mvc): adiciona informações da aplicação ausentes no configuracao.delegua

--- testes/liquido.test.ts
+++ testes/liquido.test.ts
@@ -138,20 +138,38 @@ describe('Liquido', () => {
                 );
             });
 
-            it('O valor do atributo "liquido.aplicacao.nome" deve ser o nome do projeto', async () => {
-                await copiarArquivosDeExemploParaNovoProjeto(
-                    'ProjetoLegal',
-                    'api-rest',
-                    caminhoDiretorioProjeto
-                );
+            describe('O valor do atributo "liquido.aplicacao.nome" deve ser o nome do projeto', () => {
+                it('API-REST', async () => {
+                    await copiarArquivosDeExemploParaNovoProjeto(
+                        'ProjetoLegal',
+                        'api-rest',
+                        caminhoDiretorioProjeto
+                    );
 
-                const caminhoConfiguracaoDelegua = `${caminhoDiretorioProjeto}/configuracao.delegua`;
-                const codigoConfiguracaoDelegua = await sistemaArquivos.promises.readFile(
-                    caminhoConfiguracaoDelegua,
-                    'utf-8'
-                );
+                    const caminhoConfiguracaoDelegua = `${caminhoDiretorioProjeto}/configuracao.delegua`;
+                    const codigoConfiguracaoDelegua = await sistemaArquivos.promises.readFile(
+                        caminhoConfiguracaoDelegua,
+                        'utf-8'
+                    );
 
-                expect(codigoConfiguracaoDelegua).toContain('ProjetoLegal');
+                    expect(codigoConfiguracaoDelegua).toContain('ProjetoLegal');
+                });
+
+                it('MVC', async () => {
+                    await copiarArquivosDeExemploParaNovoProjeto(
+                        'ProjetoLegal',
+                        'mvc',
+                        caminhoDiretorioProjeto
+                    );
+
+                    const caminhoConfiguracaoDelegua = `${caminhoDiretorioProjeto}/configuracao.delegua`;
+                    const codigoConfiguracaoDelegua = await sistemaArquivos.promises.readFile(
+                        caminhoConfiguracaoDelegua,
+                        'utf-8'
+                    );
+
+                    expect(codigoConfiguracaoDelegua).toContain('ProjetoLegal');
+                });
             });
 
             it('O repositório Git deve ser inicializado', async () => {
